0840ba4afb67e4956b40b3c0cda9f7d98dbb5a6e
Rainey <59759137+raineyfu@users.noreply.github.com>
2022-11-21 05:04:22 -0500

    initial

--- tests/app_test.py
+++ tests/app_test.py
@@ -5,6 +5,7 @@ from app import app
 import pytest
 import os
 from pathlib import Path
+import json
 
 TEST_DB = "test.db"
 
@@ -74,3 +75,9 @@ def test_messages(client):
     assert b"No entries here so far" not in rv.data
     assert b"&lt;Hello&gt;" in rv.data
     assert b"<strong>HTML</strong> allowed here" in rv.data
+
+def test_delete_message(client):
+    """Ensure the messages are being deleted"""
+    rv = client.get('/delete/1')
+    data = json.loads(rv.data)
+    assert data["status"] == 1
